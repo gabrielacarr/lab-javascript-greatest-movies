86eafca40beb8999f066fb11458816ef987e5bba
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-03 12:28:23 -0400

    messagesagsge

--- src/movies.js
+++ src/movies.js
@@ -1,8 +1,35 @@
 /* eslint no-restricted-globals: 'off' */
 
-// Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+// Iteration 1: Ordering by year - 
+// Order by year, ascending (in growing order)
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+const orderByYear = (arr) => {
+    let  arrCopy = [...arr];
+   
+    let sortedArray = arrCopy.sort((a,b) => {
+        if (a.year === b.year) {
+            a.title.localeCompare(b.title); 
+        } else {
+            return a.year - b.year;
+        }
+    })
+return sortedArray;
+}
+
+// This is from my macbook pro
+
+// Iteration 2: Steven Spielberg. The best? - 
+// How many drama movies did STEVEN SPIELBERG direct
+
+// Go ahead and create a `howManyMovies()` function that 
+// receives an array as a parameter and `filter` :eyes: 
+// the array so we can have only the **drama** movies where 
+// **Steven Spielberg** is the director.
+
+const owManyMovies = (arr) => {
+
+}
+
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
